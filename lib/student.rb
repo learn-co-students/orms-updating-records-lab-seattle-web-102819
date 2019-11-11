0c6199d4153db91c0d96f7531ac2a2c404f52353
4myc <missch3n@gmail.com>
2019-11-10 18:16:28 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -63,21 +63,22 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
     sql = <<-SQL
-      SELECT * 
-      FROM students 
-      WHERE name = ?
+      SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end
-  end
+    DB[:conn].execute(sql,name).map {|row| self.new_from_db(row)}.first
+  end  
   # ALTERNATIVE METHOD
   # def self.find_by_name(name)
   #   sql = "SELECT * FROM students WHERE name = ?"
   #   result = DB[:conn].execute(sql, name)[0]
   #   Student.new(result[0], result[1], result[2])
   # end
+
+  def update
+    sql = <<-SQL
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
