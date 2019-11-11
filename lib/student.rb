b1840493e5cf31f1b38b932d6fca4d6e22cd6fae
4myc <missch3n@gmail.com>
2019-11-10 18:10:05 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -63,8 +63,21 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end
   end
+  # ALTERNATIVE METHOD
+  # def self.find_by_name(name)
+  #   sql = "SELECT * FROM students WHERE name = ?"
+  #   result = DB[:conn].execute(sql, name)[0]
+  #   Student.new(result[0], result[1], result[2])
+  # end
 end
